a01af9a71663437a4613fe8d094c80c8501fa17c
ismael <fmartin@gmail.com>
2021-03-22 20:09:21 +0100

    Actividad 1 de entornos comezada

---
+++ Entornos/Actividad1.py
@@ -0,0 +1,23 @@
+TIPOA="A"
+TIPOB="B"
+PRECIOA=100
+PRECIOB=200
+TAMAÑO=1
+TAMAÑO=2
+def calculoPrecio(tipo,tamaño,cantidad):
+    precio=0
+    if tipo==TIPOA or tipo==TIPOB:
+        if tipo==TIPOA:
+            precio=PRECIOA
+        elif tipo==TIPOB:
+            precio=PRECIOB
+    else:
+
+def main():
+    precioFinal=0
+    print("Venta de Uvas")
+    tipo=input("Selecciona un tipo(A/B): ")
+    tamaño=input("Selecciona un tamaño(1/2):")
+    cantidad=input("Selecciona una cantidad: ")
+
+    print("Precio final es de "+precioFinal+"€")
